67449dd19220a7f7f6fa40dcb3dae7a0340df5c0
a7medayman6 <a.ayman6000@gmail.com>
2020-07-14 16:13:45 +0200

    done

--- Virus-Detector.py
+++ Virus-Detector.py
@@ -3,7 +3,7 @@ import hashlib
 
 ROOT = "."
 FILES = []
-DEFECTED_FILES = []
+DEFECTED = []
 VIRUSESTXT = "viruses.md5"
 
 def crawl():
@@ -14,45 +14,51 @@ def crawl():
             
             if exe: 
                 FILES.append(path)
-
-def read(file_name):
-    with open(file_name, 'r') as file:
-        try:
-            data = file.read()
-            return data
-        except Exception as ex:
-            print("Couldn't read the file.\nERROR: {}".format(ex))
-            return ""
-    return ""        
+       
 
 def scan():
     crawl()
     for file in FILES:
+        viruses = open(VIRUSESTXT, 'r')
         hasher = hashlib.md5()
         with open(file, "rb") as binary_file:
             data = binary_file.read()
             hasher.update(data)
             hash = hasher.hexdigest()
             print("file {0} md5: {1}".format(file, hash))
-            viruses = read(VIRUSESTXT)
+            
             for virus in viruses:
                 if hash == virus.strip():
                     print ("VIRUS DETECTED IN FILE {}".format(file))
-                    DEFECTED_FILES.append(file)
+                    DEFECTED.append(file)
                     act(file)
                     break
 
 def delete(path):
     try:
-        #os.remove(path)  
+        os.remove(path)  
         print("the file {} deleted successfully.")                  
     except Exception as ex:
         print("ERROR : {}".format(ex))
 
 def act(path):
-    choice = str(input("Do you want to delete this file or not? (y/n)"))
-    if choice.upper() == 'Y':
-        delete(path)
+    
+    choice = ''
+    while 1:
+        choice = str(input("Do you want to delete this file or not? (y/n):\t"))
+        if choice.upper().strip() == 'Y':
+            delete(path)
+            
+        elif choice.upper().strip() != 'N':
+            print("Wrong Choice.")
+
+        if choice.upper().strip() == 'Y' or choice.upper().strip() == 'N':
+            break        
+        print(choice.upper())        
+        
+    
+
 
 
 scan()
+print(len(DEFECTED))
